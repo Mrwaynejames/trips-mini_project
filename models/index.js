0cf7a3dac833d93c068dba7d70242c7229972ae8
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-11-03 11:42:12 -0700

    initial commit

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const Location = require('./Location');
+const Traveller = require('./Traveller');
+const Trip = require('./Trip');
+
+Traveller.hasMany(Trip, {
+  foreignKey: 'traveller_id',
+  onDelete: 'CASCADE',
+});
+
+Trip.belongsTo(Traveller, {
+  foreignKey: 'travellar_id',
+  onDelete: 'CASCADE',
+});
+
+// Define a Location as having many Tripss, thus creating a foreign key in the `Trips` table
+Location.hasMany(Trip, {
+  foreignKey: 'location_id',
+  onDelete: 'CASCADE',
+});
+
+// The association can also be created from the Trips side
+Trip.belongsTo(Location, {
+  foreignKey: 'location_id',
+  onDelete: 'CASCADE',
+});
+
+module.exports = { Location, Traveller, Trip };
